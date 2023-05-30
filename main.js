91dac27a070978867138917c79f0bf77b90e5396
Vitor Leal <vitorlealm.21@gmail.com>
2023-05-30 19:27:34 -0300

    adiciona estado basico

--- main.js
+++ main.js
@@ -2,7 +2,16 @@
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
 
+let estado = 0;
+
 function envia(event){
     event.preventDefault();
     alert("Formulario enviado");
+    estado++;
+    atualiza();
+}
+
+function atualiza(){
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${estado}</li>`;
 }
